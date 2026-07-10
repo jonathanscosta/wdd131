50c4022203b4ea82f820d6d063725885326ad794
User <thiago.kazamobile@outlook.com>
2026-07-10 13:24:24 -0300

    final da aula de DOM da semana 02

--- semana02/scripts/bom.js
+++ semana02/scripts/bom.js
@@ -1,13 +1,24 @@
 const input = document.querySelector('#favchap');
 const botao = document.querySelector('button');
-const lista = document.querySelector('list');
+const lista = document.querySelector('#list');
 
-const li = document.createElement('li');
-const botaoExcluir = document.createElement('button')
 
-li.textContent = input.value;
+botao.addEventListener('click', function () {
+    if (input.value.trim() !== "") {
+        const li = document.createElement('li');
+        const botaoExcluir = document.createElement('button')
 
-botaoExcluir.textContent = '&times;'
+        li.textContent = input.value;
+        botaoExcluir.textContent = '\u{274C}';
 
-li.append(botaoExcluir);
-lista.append(li);
+        li.append(botaoExcluir);
+        lista.append(li);
+
+        botaoExcluir.addEventListener('click', function(){
+            li.remove();
+        });
+        input.value = '';
+
+    }
+    input.focus();
+});
