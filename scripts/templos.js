d1707c9285cd4e7915b9061bd637aa13b39dea57
Jonathan Costa <costajonathan34@gmail.com>
2026-07-11 12:27:49 -0300

    ajustes no layout da página templo

--- scripts/templos.js
+++ scripts/templos.js
@@ -1,3 +1,20 @@
+
+const menuBotao = document.querySelector("#botao-menu");
+const menuNavegacao = document.querySelector(".menu");
+
+
+menuBotao.addEventListener("click", ()=>{
+    menuBotao.classList.toggle("open");
+    menuNavegacao.classList.toggle("open");
+})
+
+
+
+
+
+
+// footer
+
 const ano = document.querySelector("#anoatual");
 const ultima_modificacao = document.querySelector("#ultimaModificacao")
 
