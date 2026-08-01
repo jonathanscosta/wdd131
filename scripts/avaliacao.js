388673608cd29defbf61954d73984668c22c6c63
Jonathan Costa <costajonathan34@gmail.com>
2026-08-01 18:31:17 -0300

    criação de código do contador funcional

--- scripts/avaliacao.js
+++ scripts/avaliacao.js
@@ -1,11 +1,16 @@
 const contagem = document.querySelector("#contador");
 
 const texto = document.createElement("p");
+texto.innerHTML=""
 
 let contador = Number(localStorage.getItem("avaliacoes")) || 0;
 
-contador++;
-
+if(contador === 0){
+contador+=1
+}
+else if(contador > 0){
+    contador++
+}
 localStorage.setItem("avaliacoes", contador);
 
 texto.textContent = `Você fez ${contador} avaliações!`;
