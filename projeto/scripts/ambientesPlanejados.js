ce42d6444b34e61c5550c0112743076bd5eb32c4
Jonathan Costa <costajonathan34@gmail.com>
2026-08-12 06:52:02 -0300

    criei uma função para gerar os cards da página ambientes planejados na versão mobile

--- projeto/scripts/ambientesPlanejados.js
+++ projeto/scripts/ambientesPlanejados.js
@@ -1,10 +1,11 @@
 
 
-const galeria = [{ nome: "Cozinhas Planejadas", url: ["imagens/cozinha01.webp", "imagens/cozinha02.webp", "imagens/cozinha03.webp"] },
+const galeria = [{ nome: "Cozinha 01", url: "imagens/cozinha01.webp", preco: "R$ 20.000,00" }, { nome: "Cozinha 02", url: "imagens/cozinha02.webp", preco: "R$ 25.000,00" }, { nome: "Cozinhas 03", url: "imagens/cozinha03.webp", preco: "R$ 31.000,00" },
 
-{nome: "Salas Planejadas", url: ["imagens/sala-hero2.webp", "imagens/sala01.webp", "imagens/sala02.webp"]},
 
-{nome: "Suites Planejadas", url: ["imagens/suite01.webp", "imagens/suite02.webp", "imagens/suite03.webp"]}]
+{ nome: "Sala 01", url: "imagens/sala-hero2.webp", preco: "R$ 40.000,00" }, { nome: "Sala 02", url: "imagens/sala01.webp", preco: "R$ 18.000,00" }, { nome: "Sala 03", url: "imagens/sala02.webp", preco: "R$ 15.000,00" },
+
+{ nome: "Suite 01", url: "imagens/suite01.webp", preco: "R$ 20.000,00" }, { nome: "Suite 02", url: "imagens/suite02.webp", preco: "R$ 32.000,00" }, { nome: "Suite 03", url: "imagens/suite03.webp", preco: "R$ 12.000,00" }]
 
 const botaoMenu = document.querySelector("#botaoMenu");
 const menu = document.querySelector("#menu");
@@ -21,3 +22,37 @@ botaoMenu.addEventListener('click', () => {
         botaoMenu.setAttribute("aria-label", "abrir menu")
     }
 })
+
+
+
+
+function criarCard(array) {
+    const container = document.querySelector(".container");
+    container.innerHTML = ""
+    const titulo = document.createElement("h1");
+    titulo.textContent = "Ambientes planejados";
+    container.appendChild(titulo);
+
+    galeria.forEach(ambientes=>{
+
+    const card = document.createElement("section");
+    const titulo2 = document.createElement("h2");
+    const img = document.createElement("img");
+    const preco = document.createElement("p")
+
+    titulo2.textContent = ambientes.nome
+    img.src = ambientes.url;
+    img.alt = ambientes.nome;
+    preco.innerHTML = `<strong>${ambientes.preco}</strong>`;
+    
+    
+    card.appendChild(titulo2);
+    card.appendChild(img);
+    card.appendChild(preco);
+    
+    container.append(card);
+    })
+    
+}
+
+criarCard(galeria);
