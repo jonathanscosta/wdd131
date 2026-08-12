27354f69a57a2b6b92d0d842628688b5d090bdd7
Jonathan Costa <costajonathan34@gmail.com>
2026-08-11 22:16:20 -0300

    inicio da criação das páginas fale conosco e ambientes planejados

---
+++ projeto/scripts/ambientesPlanejados.js
@@ -0,0 +1,8 @@
+
+
+const galeria = [{ nome: "Cozinhas Planejadas", url: ["imagens/cozinha01.webp", "imagens/cozinha02.webp", "imagens/cozinha03.webp"] },
+
+{nome: "Salas Planejadas", url: ["imagens/sala-hero2.webp", "imagens/sala01.webp", "imagens/sala02.webp"]},
+
+{nome: "Suites Planejadas", url: ["imagens/suite01.webp", "imagens/suite02.webp", "imagens/suite03.webp"]}]
+
